360b49120bfef8066557381cf2f22f9fab27d0ad
Derek Wang <derek.wang@hpe.com>
2020-03-11 19:29:23 -0700

    "updated the setup.py file with the public github link"

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import find_packages
 setup(name='pyaoscx',
       version='0.1.0',
       description='AOS-CX Python Modules',
-      url='https://github.com/aruba/aos-cx-python',
-      author='Aruba Switching Automatoin',
+      url='https://github.com/aruba/pyaoscx',
+      author='Aruba Switching Automation',
       author_email='aruba-switching-automation@hpe.com',
       license='Apache 2.0',
     classifiers=[
@@ -19,7 +19,7 @@ setup(name='pyaoscx',
 
         'Programming Language :: Python :: 3 :: Only'
     ],
-    keywords='networking aruba aos-cx switch rest api',
+    keywords='networking aruba aos-cx switch rest api python',
     packages=find_packages(exclude=['docs']),
     install_requires=['requests', 'PyYAML'],
       zip_safe=False)
