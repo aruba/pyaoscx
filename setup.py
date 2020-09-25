8dd65f38203d6f3f06b7c4bc7724e6c6573c0c38
Derek Wang <47010244+derekwangHPEAruba@users.noreply.github.com>
2020-09-25 12:40:04 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='pyaoscx',
-      version='0.2.0',
+      version='0.2.1',
       description='AOS-CX Python Modules',
       long_description=long_description,
       long_description_content_type='text/markdown',
