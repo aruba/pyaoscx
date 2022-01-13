0e57c62a19403641c178d2e90ea8354b9becd2e7
David Cordero Chavarria <david.cordero@hpe.com>
2022-01-13 08:19:06 -0800

    Fix selector assignment order
    A previous change left the QoS CoS module's get() without a selector
    assignment before its use
    
    This CL fixes this issue
    
    CR 210983
    
    Change-Id: I4bdc86eaacce95a20698a38654e60fe05a35d056

--- pyaoscx/qos_cos.py
+++ pyaoscx/qos_cos.py
@@ -71,10 +71,10 @@ class QosCos(PyaoscxModule):
         logging.info(
             "Retrieving the switch %s QoS COS trust mode.", self.code_point)
 
-        data = self._get_data(depth, selector)
-
         depth = depth or self.session.api.default_depth
         selector = selector or self.session.api.default_selector
+
+        data = self._get_data(depth, selector)
 
         # Add dictionary as attributes for the object
         utils.create_attrs(self, data)
