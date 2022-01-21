b5d95e0954f34b8f23661de12895522ff3abc0f2
David Cordero Chavarria <david.cordero@hpe.com>
2022-01-21 15:32:02 -0800

    Fix GenericOperationError Exception
    The GenericOperationError class is inconsistent in the number of
    arguments it requires, it works with either 0, 2, or 3 arguments, but
    not only 1, if passed only 1, it raises an Exception.
    
    This CL fixes it so it accepts 0, 1, 2, or 3 arguments, removes an extra
    quote character from its dosctring, simplifies its docstring
    description, and simplifies how its message is constructed, after
    validating its arguments.
    
    Change-Id: Id75575bde8e31b778e50db4dc59cc3571cab9d4a

--- pyaoscx/exceptions/generic_op_error.py
+++ pyaoscx/exceptions/generic_op_error.py
@@ -1,34 +1,35 @@
-# (C) Copyright 2019-2021 Hewlett Packard Enterprise Development LP.
+# (C) Copyright 2019-2022 Hewlett Packard Enterprise Development LP.
 # Apache License 2.0
 
 from pyaoscx.exceptions.pyaoscx_error import PyaoscxError
 
 
 class GenericOperationError(PyaoscxError):
-    """"
-    Class used to add information regarding a Generic Operation Error
-    inside PYAOSCX
+    """
+    PYAOSCX Generic Operation Error Exception
     """
 
     def __init__(self, *args):
+        self.message = None
+        self.response_code = None
+        self.extra_info = None
         if args:
             super().__init__(args[0])
-            self.response_code = args[1]
-            try:
-                self.extra_info = args[2]
-            except IndexError:
-                self.extra_info = None
-        else:
-            self.message = None
-            self.extra_info = None
+            if len(args) >= 2:
+                self.response_code = args[1]
+            if len(args) > 2:
+                self.extra_info = ", ".join(str(a) for a in args[2:])
 
     def __str__(self):
-        if self.message and self.extra_info:
-            return "GENERIC OPERATION ERROR: {0} Code: {1}"\
-                " on Module {2}".format(
-                    self.message, self.response_code, self.extra_info)
+        base_msg = "GENERIC OPERATION ERROR"
+        msg_parts = [base_msg]
         if self.message:
-            return "GENERIC OPERATION ERROR: {0} Code: {1}".format(
-                self.message, self.response_code)
-        else:
-            return "GENERIC OPERATION ERROR"
+            msg_parts.append(self.message)
+        if self.response_code:
+            msg_parts.append("Code")
+            msg_parts.append(self.response_code)
+        if self.extra_info:
+            msg_parts.append("on Module")
+            msg_parts.append(self.extra_info)
+        msg = ": ".join(msg_parts)
+        return msg
